4e5bdd6ec6556faf6f64b92af81a144927015e44
yeahsilver <gjdpdms2005@gmail.com>
2021-12-28 22:33:08 +0900

    개방/폐쇄 원칙

--- solid_practice/solid_practice/main.swift
+++ solid_practice/solid_practice/main.swift
@@ -5,6 +5,8 @@
 //  Created by 허예은 on 2021/12/26.
 //
 
+// 참조: https://velog.io/@delmasong/Swift-개발에-SOLID-적용하기
+
 import Foundation
 
 // 1. 단일 책임 원칙
@@ -16,3 +18,5 @@ loginAfter.login()
 
 // 2. 개방/폐쇄원칙: 확장은 클래스의 행동 변화 없이 가능해야한다. (확장에는 열려있고 변경에는 닫혀있어야 한다.)
 
+let vehicle = Vehicle()
+vehicle.setVehicle()
--- solid_practice/solid_practice/second.swift
+++ solid_practice/solid_practice/second.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+class Vehicle {
+    func setVehicle() {
+        print("Set Vehicle")
+        let cars = [Car(brand: "Hyudai"),
+                    Car(brand: "KIA"),
+                    Car(brand: "Renault")
+        ]
+        
+        let trucks = [Truck(brand: "Hyudai"),
+                      Truck(brand: "KIA")
+        ]
+        
+        for car in cars {
+            car.getCarName()
+        }
+        
+        for truck in trucks {
+            truck.getCarName()
+        }
+    }
+}
+
+class Car {
+    let brand: String
+    
+    init(brand: String) {
+        self.brand = brand
+    }
+    
+    func getCarName() {
+        print("car: \(brand)")
+    }
+}
+
+class Truck {
+    let brand: String
+    
+    init(brand: String) {
+        self.brand = brand
+    }
+    
+    func getCarName() {
+        print("truck: \(brand)")
+    }
+}
