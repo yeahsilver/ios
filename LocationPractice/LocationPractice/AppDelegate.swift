60be57a9b9362273fe56596ea3cadb99539e2410
yeahsilver <gjdpdms2005@gmail.com>
2022-01-04 00:27:15 +0900

    add tasks

--- LocationPractice/LocationPractice/AppDelegate.swift
+++ LocationPractice/LocationPractice/AppDelegate.swift
@@ -31,6 +31,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        application.beginBackgroundTask(withName: "") {}
+    }
 }
 
