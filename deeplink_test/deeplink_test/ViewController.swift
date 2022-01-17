24c5a5de086db96968266bd5221c727c9eb2cba6
yeahsilver <gjdpdms2005@gmail.com>
2022-01-17 22:02:02 +0900

    create deeplink test

---
+++ deeplink_test/deeplink_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  deeplink_test
+//
+//  Created by mac on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
