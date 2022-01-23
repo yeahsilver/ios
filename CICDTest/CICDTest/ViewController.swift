1b3328f1b74e52acf8c2961ff40f5f18c4bd0235
yeahsilver <gjdpdms2005@gmail.com>
2022-01-23 16:20:48 +0900

    create a project

---
+++ CICDTest/CICDTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDTest
+//
+//  Created by mac on 2022/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
