03e67e5bc8a92dcde10b9eebf6b96d7808089d71
yeahsilver <gjdpdms2005@gmail.com>
2022-02-03 23:36:59 +0900

    binding

--- ErrorHandlingPractice/ErrorHandlingPractice/ViewController.swift
+++ ErrorHandlingPractice/ErrorHandlingPractice/ViewController.swift
@@ -21,6 +21,45 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    private func piechartBind() {
+        viewModel.piechart
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] result in
+                switch result {
+                case .success(let data):
+                    self?.dailyData = data
+                    self?.summaryCollectionView.reloadData()
+                case .requestErr:
+                    self?.piechartErrorData()
+                case .pathErr:
+                    self?.piechartErrorData()
+                case .serverErr:
+                    self?.piechartErrorData()
+                case .networkFail:
+                    self?.piechartErrorData()
+                }
+            }, onError: { [weak self] result in
+                self?.settingNetworkAlert()
+                self?.piechartErrorData()
+            }).disposed(by: disposeBag)
+    }
 }
 
+
+class ViewModel {
+    func getMap(date: String) {
+        service.getMap(date: date)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] result in
+                WolleyLog.debug("GET /map result >>", result)
+                self?.map.onNext(result)
+                
+            }, onError: { [weak self] error in
+                self?.map.onError(error)
+                WolleyLog.debug("GET /map error >>", error)
+                
+            }).disposed(by: disposeBag)
+    }
+}
 
