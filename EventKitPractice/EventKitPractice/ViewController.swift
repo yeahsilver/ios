2e996fa49684a0ddeeb4dda436646dea3dbf92fe
ella-dev <39258902+yeahsilver@users.noreply.github.com>
2022-02-16 18:54:08 +0900

    event kit commit

---
+++ EventKitPractice/EventKitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventKitPractice
+//
+//  Created by mac on 2022/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
